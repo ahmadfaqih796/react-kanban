8d0857026cfd268ee26422d0be6490d8a11956e3
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-07-06 15:59:54 +0700

    use router hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "antd";
+import { HOMEPAGE } from "vite-env";
+import Test from "./pages/Test";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Test />
+      <button type="primary">Login</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Test.jsx
@@ -0,0 +1,9 @@
+import { useRouter } from "@/router/hooks";
+import React from "react";
+
+const Test = () => {
+  const router = useRouter();
+  return <div>Test</div>;
+};
+
+export default Test;
