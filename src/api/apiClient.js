a1ce207e334b5dc9dbf5d852b9a752f09345bb91
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-07-06 16:13:54 +0700

    feat: use store

---
+++ src/api/apiClient.js
@@ -0,0 +1,91 @@
+import { message as Message } from "antd";
+import axios from "axios";
+
+import { t } from "@/locales/i18n";
+import userStore from "@/store/userStore";
+
+import { ResultEnum } from "#/enum";
+
+// Membuat instance axios
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API,
+  timeout: 50000,
+  headers: { "Content-Type": "application/json;charset=utf-8" },
+});
+
+// Interceptor untuk permintaan
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // Sebelum mengirimkan permintaan, lakukan hal ini
+    config.headers.Authorization = "Bearer Token";
+    return config;
+  },
+  (error) => {
+    // Ketika terjadi kesalahan pada permintaan
+    return Promise.reject(error);
+  }
+);
+
+// Interceptor untuk tanggapan
+axiosInstance.interceptors.response.use(
+  (res) => {
+    if (!res.data) throw new Error(t("sys.api.apiRequestFailed"));
+
+    const { status, data, message } = res.data;
+    // Jika permintaan bisnis berhasil
+    const hasSuccess =
+      data && Reflect.has(res.data, "status") && status === ResultEnum.SUCCESS;
+    if (hasSuccess) {
+      return data;
+    }
+
+    // Jika terjadi kesalahan pada permintaan bisnis
+    throw new Error(message || t("sys.api.apiRequestFailed"));
+  },
+  (error) => {
+    const { response, message } = error || {};
+
+    const errMsg =
+      response?.data?.message || message || t("sys.api.errorMessage");
+    Message.error(errMsg);
+
+    const status = response?.status;
+    if (status === 401) {
+      userStore.getState().actions.clearUserInfoAndToken();
+    }
+    return Promise.reject(error);
+  }
+);
+
+class APIClient {
+  get(config) {
+    return this.request({ ...config, method: "GET" });
+  }
+
+  post(config) {
+    return this.request({ ...config, method: "POST" });
+  }
+
+  put(config) {
+    return this.request({ ...config, method: "PUT" });
+  }
+
+  delete(config) {
+    return this.request({ ...config, method: "DELETE" });
+  }
+
+  request(config) {
+    return new Promise((resolve, reject) => {
+      axiosInstance
+        .request(config)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  }
+}
+
+export default new APIClient();
---
+++ src/store/userStore.js
@@ -0,0 +1,63 @@
+import { useMutation } from "@tanstack/react-query";
+import { App } from "antd";
+import { useNavigate } from "react-router-dom";
+import { create } from "zustand";
+
+import userService from "@/api/services/userService";
+import { getItem, removeItem, setItem } from "@/utils/storage";
+
+import { StorageEnum } from "#/enum";
+import { HOMEPAGE } from "vite-env";
+
+const useUserStore = create((set) => ({
+  userInfo: getItem(StorageEnum.User) || {},
+  userToken: getItem(StorageEnum.Token) || {},
+  actions: {
+    setUserInfo: (userInfo) => {
+      set({ userInfo });
+      setItem(StorageEnum.User, userInfo);
+    },
+    setUserToken: (userToken) => {
+      set({ userToken });
+      setItem(StorageEnum.Token, userToken);
+    },
+    clearUserInfoAndToken() {
+      set({ userInfo: {}, userToken: {} });
+      removeItem(StorageEnum.User);
+      removeItem(StorageEnum.Token);
+    },
+  },
+}));
+
+export const useUserInfo = () => useUserStore((state) => state.userInfo);
+export const useUserToken = () => useUserStore((state) => state.userToken);
+export const useUserPermission = () =>
+  useUserStore((state) => state.userInfo.permissions);
+export const useUserActions = () => useUserStore((state) => state.actions);
+
+export const useSignIn = () => {
+  const navigate = useNavigate();
+  const { message } = App.useApp();
+  const { setUserToken, setUserInfo } = useUserActions();
+
+  const signInMutation = useMutation(userService.signin);
+
+  const signIn = async (data) => {
+    try {
+      const res = await signInMutation.mutateAsync(data);
+      const { user, accessToken, refreshToken } = res;
+      setUserToken({ accessToken, refreshToken });
+      setUserInfo(user);
+      navigate(HOMEPAGE, { replace: true });
+    } catch (err) {
+      message.warning({
+        content: err.message,
+        duration: 3,
+      });
+    }
+  };
+
+  return signIn;
+};
+
+export default useUserStore;
