5dadaf1e0f09b46378e72b2408613ef244ac28f4
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-07-20 11:54:32 +0700

    return button login

---
+++ src/pages/sys/login/components/ReturnButton.jsx
@@ -0,0 +1,15 @@
+import { Button } from "antd";
+import { useTranslation } from "react-i18next";
+import { MdArrowBackIosNew } from "react-icons/md";
+
+export function ReturnButton({ onClick }) {
+  const { t } = useTranslation();
+  return (
+    <Button block type="link" onClick={onClick}>
+      <div className="flex items-center justify-center hover:underline">
+        <MdArrowBackIosNew />
+        <span className="text-sm">{t("sys.login.backSignIn")}</span>
+      </div>
+    </Button>
+  );
+}
